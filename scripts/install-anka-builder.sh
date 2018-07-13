6a51dbe640431a6feddaf7eae4e051e26982cf28
fedor <fedor.korotkov@gmail.com>
2018-07-13 13:47:44 -0400

    fixed url

--- scripts/install-anka-builder.sh
+++ scripts/install-anka-builder.sh
@@ -5,7 +5,7 @@ set -e
 BUILDER_VERSION=1.0.1
 
 curl -L -o packer-builder-veertu-anka.tar.gz \
-     https://github.com/buildkite/packer-builder-veertu-anka/releases/download/v${BUILDER_VERSION}/packer-builder-veertu-anka_${BUILDER_VERSION}_darwin_amd64.tar.gz
+     https://github.com/veertuinc/packer-builder-veertu-anka/releases/download/v${BUILDER_VERSION}/packer-builder-veertu-anka_${BUILDER_VERSION}_darwin_amd64.tar.gz
 
 mkdir -p build
 tar -xvzf packer-builder-veertu-anka.tar.gz -C build
