4a63288a7f3f0b66874683172b916baac02e8d98
Reece Dunham <me@rdil.rocks>
2019-11-07 17:42:22 +0000

    Removed tests, outdated, and templates

--- scripts/testing/run-performance-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ANKA_VERSION=$(anka version | egrep -o '([0-9].)+[0-9]')
-
-time anka run --volume $PWD/scripts/testing/volume high-sierra-perf-$ANKA_VERSION ./performance-test.sh
