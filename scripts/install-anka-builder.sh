7bab47062989987e26124970b72dddfa21e56db1
fkorotkov <fedor.korotkov@gmail.com>
2020-10-13 16:58:17 -0400

    Fixed link

--- scripts/install-anka-builder.sh
+++ scripts/install-anka-builder.sh
@@ -5,7 +5,7 @@ set -e
 BUILDER_VERSION=1.5.0
 
 curl -L -o packer-builder-veertu-anka.zip \
-     https://github.com/veertuinc/packer-builder-veertu-anka/releases/download/v${BUILDER_VERSION}/packer-builder-veertu-anka.zip
+     https://github.com/veertuinc/packer-builder-veertu-anka/releases/download/v${BUILDER_VERSION}/packer-builder-veertu-anka_v${BUILDER_VERSION}_darwin_amd64.zip
 
 mkdir -p build
 unzip packer-builder-veertu-anka.zip
